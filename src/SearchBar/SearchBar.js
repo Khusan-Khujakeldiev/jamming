cde774a0da07fec2c2d05393b9f28952fb535686
Khusan-Khujakeldiev <Akatsuki-xus@mail.ru>
2024-05-05 20:17:08 +0300

    Start to make SearchBar and Add input for search

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,15 +1,56 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./SearchBar.css";
 
-function SearchBar() {
+function MyComponent() {
+  const iconRef = useRef(null);
+  const inputRef = useRef(null);
+  const [input, setInput] = useState("");
+  const [isActive, setIsActive] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+  const handleFocus = () => {
+    setIsActive(true);
+  };
+  const handleBlur = (e) => {
+    if (e.target.value.length === 0) setIsActive(false);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsProcessing(true);
+    setIsActive(false);
+    inputRef.current.disabled = true;
+    setTimeout(() => {
+      setIsProcessing(false);
+      inputRef.current.disabled = false;
+      if (input.length > 0) {
+        setIsActive(true);
+      }
+    }, 1000);
+  };
+
   return (
     <div className="container">
-      <form autocomplete="off">
-        <div className="finder">
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <div
+          className={`finder ${isActive ? "active" : ""} ${
+            isProcessing ? "processing" : ""
+          }`}
+        >
           <div className="finder__outer">
             <div className="finder__inner">
-              <div className="finder__icon" ref="icon"></div>
-              <input className="finder__input" type="text" name="q" />
+              <div className="finder__icon" ref={iconRef}></div>
+              <input
+                ref={inputRef}
+                className="finder__input"
+                type="text"
+                name="q"
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                onChange={handleInputChange}
+              />
             </div>
           </div>
         </div>
@@ -18,4 +59,4 @@ function SearchBar() {
   );
 }
 
-export default SearchBar;
+export default MyComponent;
