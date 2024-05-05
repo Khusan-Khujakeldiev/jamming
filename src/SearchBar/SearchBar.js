9fa52298f583f22f9016173a10b683883600c8ad
Khusan-Khujakeldiev <Akatsuki-xus@mail.ru>
2024-05-05 18:05:14 +0300

    Add new input label for search

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./SearchBar.css";
 
 function SearchBar() {
-  return <div className="SearchBar"></div>;
+  return (
+    <div className="container">
+      <form autocomplete="off">
+        <div className="finder">
+          <div className="finder__outer">
+            <div className="finder__inner">
+              <div className="finder__icon" ref="icon"></div>
+              <input className="finder__input" type="text" name="q" />
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
