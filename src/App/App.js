b67ffe51081be62d62ceaa5775251536eba0bf9d
Khusan-Khujakeldiev <Akatsuki-xus@mail.ru>
2024-05-06 18:47:11 +0300

    Start to move to Scss

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 import SearchBar from "../SearchBar/SearchBar";
-import "./App.css";
+import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <h1 className="unselectable">
+      <h1 className="unSelectable">
         Ja<span className="highlight">mmm</span>ing
       </h1>
       <SearchBar></SearchBar>
